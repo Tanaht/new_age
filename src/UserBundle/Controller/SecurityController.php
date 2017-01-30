516fee63df010892421f51121fd5eb3e3ec30778
Tanaky <charp.antoine@gmail.com>
2017-01-30 15:39:03 +0100

    /connexion2: revert of "Composer update" and feature finished
    Feature fini, la connexion est fonctionnelle (user stocker en bdd) et la fonctionnalité de remember me également.
    
    Impossibilité pour un utilisation non connecté d'acceder aux urls de la forme: /app/*

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityController extends Controller
+{
+    public function connexionAction()
+    {
+        if($this->get('security.authorization_checker')->isGranted("IS_AUTHENTICATED_REMEMBERED")){
+            return $this->redirectToRoute("home");//TODO: redirect to mainpage when authenticated
+        }
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('@User/Default/connexion_form.html.twig', array(
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        ));
+    }
+
+    public function temp_routeAction()
+    {
+        return new Response("<body>Hello Authenticated user !!!</body>");
+    }
+}
