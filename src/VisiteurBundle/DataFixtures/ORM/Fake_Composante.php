0e4db55bb089908d4e63ae786a87898b8bdf0927
Antoine Mullier <antoinemullier@gmail.com>
2017-01-31 19:12:49 +0100

    Creation d'une data-fixture pour les composantes

---
+++ src/VisiteurBundle/DataFixtures/ORM/Fake_Composante.php
@@ -0,0 +1,42 @@
+<?php
+namespace UserBundle\DataFixtures;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use VisiteurBundle\Entity\Composante;
+
+/**
+ * Création des composantes pour le développement
+ */
+class Fake_Composante implements FixtureInterface
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $composantes = new ArrayCollection();
+
+        $composantes->add(array(
+            'nom' => 'ISTIC'
+        ));
+
+
+        $composantes->add(array(
+            'nom' => 'ESIR'
+        ));
+
+
+        $composantes->forAll(function($index, array $info) use($manager) {
+            $composante = new Composante();
+            $composante->setNom($info['nom']);
+            
+            $manager->persist($composante);
+            return true;
+        });
+        $manager->flush();
+    }
+}
