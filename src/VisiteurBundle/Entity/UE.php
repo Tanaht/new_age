e3055b4dd4197fc5b63ed63a63fbfc4e3d860ecc
Unknown <erwann.berjeaud@gmail.com>
2017-02-01 15:35:32 +0100

    Creation entité UE

---
+++ src/VisiteurBundle/Entity/UE.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace VisiteurBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UE
+ *
+ * @ORM\Table(name="u_e")
+ * @ORM\Entity(repositoryClass="VisiteurBundle\Repository\UERepository")
+ */
+class UE
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return UE
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
