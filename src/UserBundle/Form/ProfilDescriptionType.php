e6d17fd0bb11ed3d023e7b755f64e20c9dda94b6
Tanaky <charp.antoine@gmail.com>
2017-02-16 13:17:31 +0100

    Updating description form complete

---
+++ src/UserBundle/Form/ProfilDescriptionType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilDescriptionType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description', TextareaType::class, [
+            'label' => false,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer la description',
+                'attr' => ['class' => 'btn-primary'],
+            ])
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\Utilisateur'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_utilisateur';
+    }
+
+
+}
--- src/VisiteurBundle/Controller/ProfilController.php
+++ src/VisiteurBundle/Controller/ProfilController.php
@@ -2,8 +2,12 @@
 
 namespace VisiteurBundle\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\Utilisateur;
+use UserBundle\Form\ProfilDescriptionType;
 use UserBundle\Form\ProfilGeneralInformationsType;
 use UserBundle\Form\UtilisateurType;
 use VisiteurBundle\Entity\Email;
@@ -14,32 +18,60 @@ use VisiteurBundle\Entity\NumeroTelephone;
  */
 class ProfilController extends Controller
 {
+    private function handleProfilGeneraleInformationsForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om) {
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $om = $this->getDoctrine()->getManager();
+
+            $om->persist($utilisateur);
+            $utilisateur->getEmailList()->forAll(function($index,Email $email) use($om,$utilisateur)  {
+                $email->setUser($utilisateur);
+                $om->persist($email);
+                return true;
+            });
+            $utilisateur->getNumList()->forAll(function($index,NumeroTelephone $numero) use($om,$utilisateur)  {
+                $numero->setUser($utilisateur);
+                $om->persist($numero);
+                return true;
+            });
+            $om->flush();
+            return true;
+        }
+        return false;
+    }
+
+    private function handleProfilDescriptionForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om)
+    {
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $om->persist($utilisateur);
+            $om->flush();
+            return true;
+        }
+        return false;
+    }
+
     public function monProfilAction(Request $request)
     {
         $utilisateur = $this->getUser();
-        $form = $this->createForm(ProfilGeneralInformationsType::class, $utilisateur, ['action' => $this->generateUrl('visiteur_homepage')]);
+
+        $profilGeneralInformationsform = $this->createForm(ProfilGeneralInformationsType::class, $utilisateur, ['action' => $request->getUri()]);
+        $profilDescriptionForm = $this->createForm(ProfilDescriptionType::class, $utilisateur, ['action' => $request->getUri()]);
+
+        $om = $this->getDoctrine()->getManager();
 
         if($request->isMethod('POST')) {
-            $form->handleRequest($request);
-            if ($form->isSubmitted() && $form->isValid()) {
-                $em = $this->getDoctrine()->getManager();
+            if($this->handleProfilGeneraleInformationsForm($request, $profilGeneralInformationsform, $utilisateur, $om))
+                return $this->redirectToRoute("visiteur_homepage");
 
-                $em->persist($utilisateur);
-                $utilisateur->getEmailList()->forAll(function($index,Email $email) use($em,$utilisateur)  {
-                    $email->setUser($utilisateur);
-                    $em->persist($email);
-                    return true;
-                });
-                $utilisateur->getNumList()->forAll(function($index,NumeroTelephone $numero) use($em,$utilisateur)  {
-                    $numero->setUser($utilisateur);
-                    $em->persist($numero);
-                    return true;
-                });
-                $em->flush();
-                return $this->redirect($request->getUri());
-            }
+            if($this->handleProfilDescriptionForm($request, $profilDescriptionForm, $utilisateur, $om))
+                return $this->redirectToRoute("visiteur_homepage");
         }
 
-        return $this->render("@Visiteur/Default/mon_profil.html.twig", ['profilGeneralInformationsForm' => $form->createView()]);
+        return $this->render("@Visiteur/Default/mon_profil.html.twig", [
+            'profilGeneralInformationsForm' => $profilGeneralInformationsform->createView(),
+            'profilDescriptionForm' => $profilDescriptionForm->createView(),
+        ]);
     }
 }
