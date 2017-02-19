2890a61d694fd1be55afc1820d677351b077928a
Tanaky <charp.antoine@gmail.com>
2017-02-19 04:15:31 +0100

    Adding flashBag support for Form Error
    And links in flashbag warning to open relative form modal

--- src/VisiteurBundle/Controller/ProfilController.php
+++ src/VisiteurBundle/Controller/ProfilController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use UserBundle\Entity\Utilisateur;
 use UserBundle\Form\ProfilDescriptionType;
 use UserBundle\Form\ProfilGeneralInformationsType;
@@ -20,89 +21,115 @@ use VisiteurBundle\Entity\NumeroTelephone;
  */
 class ProfilController extends Controller
 {
-    private function handleProfilGeneraleInformationsForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om) {
+    private function handleProfilGeneraleInformationsForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om, $modalTarget) {
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $om = $this->getDoctrine()->getManager();
 
-            $om->persist($utilisateur);
-            $utilisateur->getEmailList()->forAll(function($index,Email $email) use($om,$utilisateur)  {
-                $om->persist($email);
+        if ($form->isSubmitted()) {
+            if($form->isValid()) {
+                $om = $this->getDoctrine()->getManager();
+
+                $om->persist($utilisateur);
+                $utilisateur->getEmailList()->forAll(function($index,Email $email) use($om,$utilisateur)  {
+                    $om->persist($email);
+                    return true;
+                });
+                $utilisateur->getNumList()->forAll(function($index,NumeroTelephone $numero) use($om,$utilisateur)  {
+                    $om->persist($numero);
+                    return true;
+                });
+                $om->flush();
                 return true;
-            });
-            $utilisateur->getNumList()->forAll(function($index,NumeroTelephone $numero) use($om,$utilisateur)  {
-                $om->persist($numero);
-                return true;
-            });
-            $om->flush();
-            return true;
+            }
+            $flashBag = $request->getSession()->getFlashBag();
+            $flashBag->add('warning', "<a data-toggle='modal' href='#$modalTarget'>Une erreur est survenue sur le formulaire général</a>");
         }
+
         return false;
     }
 
     private function handleProfilDescriptionForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om)
     {
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted()) {
+            if($form->isValid()) {
+                $om->persist($utilisateur);
+                $om->flush();
+                return true;
+            }
 
-            $om->persist($utilisateur);
-            $om->flush();
-            return true;
+            $flashBag = $request->getSession()->getFlashBag();
+            $flashBag->add('warning', 'Une erreur est survenue sur le formulaire de description');
         }
+
         return false;
     }
 
     private function handleProfilImageForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om)
     {
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
 
-            // $file stores the uploaded PDF file
-            /** @var UploadedFile $file */
-            $file = $utilisateur->getFile();
-            dump($file);
+        if($form->isSubmitted()) {
+            if ($form->isValid()) {
+                // $file stores the uploaded PDF file
+                /** @var UploadedFile $file */
+                $file = $utilisateur->getFile();
+                dump($file);
 
-            // Generate a unique name for the file before saving it
-            $imageName = md5(uniqid()).'.'.$file->guessExtension();
+                // Generate a unique name for the file before saving it
+                $imageName = md5(uniqid()).'.'.$file->guessExtension();
 
-            // Move the file to the directory where brochures are stored
-            $file->move(
-                $this->getParameter('images_directory'),
-                $imageName
-            );
+                // Move the file to the directory where brochures are stored
+                $file->move(
+                    $this->getParameter('images_directory'),
+                    $imageName
+                );
 
-            // Update the 'brochure' property to store the PDF file name
-            // instead of its contents
-            $utilisateur->setImage($imageName);
+                // Update the 'brochure' property to store the PDF file name
+                // instead of its contents
+                $utilisateur->setImage($imageName);
 
-            $om->persist($utilisateur);
-            $om->flush();
-            return true;
+                $om->persist($utilisateur);
+                $om->flush();
+                return true;
+            }
+
+            $flashBag = $request->getSession()->getFlashBag();
+            $flashBag->add('warning', 'Une erreur est survenue sur le formulaire d\'upload');
         }
         return false;
     }
 
-    private function handleProfilPasswordForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om)
+    private function handleProfilPasswordForm(Request $request, Form $form, Utilisateur $utilisateur, ObjectManager $om, $modalTarget)
     {
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            //retrieve password string in profilPasswordForm arborescence
-            $newPassword = $form->get('password')->get('first')->getData();
 
-            $encoder = $this->get('security.encoder_factory')->getEncoder($utilisateur);
+        if($form->isSubmitted()) {
+            if ($form->isValid()) {
+                //retrieve password string in profilPasswordForm arborescence
+                $newPassword = $form->get('password')->get('first')->getData();
 
-            $utilisateur->setPassword($encoder->encodePassword($newPassword, null));
+                $encoder = $this->get('security.encoder_factory')->getEncoder($utilisateur);
 
-            $om->persist($utilisateur);
-            $om->flush();
-            return true;
+                $utilisateur->setPassword($encoder->encodePassword($newPassword, null));
+
+                $om->persist($utilisateur);
+                $om->flush();
+                return true;
+            }
+
+            $flashBag = $request->getSession()->getFlashBag();
+            $flashBag->add('warning', "<a data-toggle='modal' href='#$modalTarget'>Une erreur est survenue sur le formulaire de modification du mot de passe</a>");
         }
+
         return false;
     }
 
     public function monProfilAction(Request $request)
     {
         $utilisateur = $this->getUser();
+
+        $profilGeneralInformationsFormModalTarget = 'modif-profil-general';
+        $profilPasswordFormModalTarget = 'modif-profil-mdp';
 
         $profilGeneralInformationsform = $this->createForm(ProfilGeneralInformationsType::class, $utilisateur, ['action' => $request->getUri()]);
         $profilDescriptionForm = $this->createForm(ProfilDescriptionType::class, $utilisateur, ['action' => $request->getUri()]);
@@ -112,13 +139,13 @@ class ProfilController extends Controller
         $om = $this->getDoctrine()->getManager();
 
         if($request->isMethod('POST')) {
-            if($this->handleProfilGeneraleInformationsForm($request, $profilGeneralInformationsform, $utilisateur, $om))
+            if($this->handleProfilGeneraleInformationsForm($request, $profilGeneralInformationsform, $utilisateur, $om, $profilGeneralInformationsFormModalTarget))
                 return $this->redirectToRoute("visiteur_homepage");//POST REDIRECT GET (see: https://fr.wikipedia.org/wiki/Post-redirect-get)
 
             if($this->handleProfilDescriptionForm($request, $profilDescriptionForm, $utilisateur, $om))
                 return $this->redirectToRoute("visiteur_homepage");//POST REDIRECT GET (see: https://fr.wikipedia.org/wiki/Post-redirect-get)
 
-            if($this->handleProfilPasswordForm($request, $profilPasswordForm, $utilisateur, $om))
+            if($this->handleProfilPasswordForm($request, $profilPasswordForm, $utilisateur, $om, $profilPasswordFormModalTarget))
                 return $this->redirectToRoute("visiteur_homepage");//POST REDIRECT GET (see: https://fr.wikipedia.org/wiki/Post-redirect-get)
 
             if($this->handleProfilImageForm($request, $profilImageForm, $utilisateur, $om))
@@ -126,6 +153,8 @@ class ProfilController extends Controller
         }
 
         return $this->render("@Visiteur/Default/mon_profil.html.twig", [
+            'profilGeneralInformationsFormModalTarget' => $profilGeneralInformationsFormModalTarget,
+            'profilPasswordFormModalTarget' => $profilPasswordFormModalTarget,
             'profilGeneralInformationsForm' => $profilGeneralInformationsform->createView(),
             'profilDescriptionForm' => $profilDescriptionForm->createView(),
             'profilPasswordForm' => $profilPasswordForm->createView(),
