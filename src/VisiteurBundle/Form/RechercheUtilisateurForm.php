ddacc850d1c43b5906002a46c2abc305c58b5c35
Tanaky <charp.antoine@gmail.com>
2017-03-01 14:55:58 +0100

    Fix Typeahead

--- src/VisiteurBundle/Form/RechercheUtilisateurForm.php
+++ src/VisiteurBundle/Form/RechercheUtilisateurForm.php
@@ -12,7 +12,14 @@ class RechercheUtilisateurForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom', TextType::class, array("label"=>"Nom de l'utilisateur"))
+            ->add('nom', TextType::class, array(
+                "label"=>"Nom de l'utilisateur",
+                "attr" => [
+                    "typeahead" => "typeahead",
+                    "display" => "'username'",
+                    "url" => "'/new_age/web/app_dev.php/api/utilisateurs'",
+                ]
+            ))
             ->add('rechercher',SubmitType::class, array("label"=>"Rechercher"));
     }
 
