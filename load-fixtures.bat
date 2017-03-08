ee0c72e4d195d9be746d2d873d656a9f7188edfe
Antoine Mullier <antoinemullier@gmail.com>
2017-03-08 14:54:07 +0100

    Mise à jour des scripts de mise à jour des datas

--- load-fixtures.bat
+++ load-fixtures.bat
@@ -1,3 +1,11 @@
+#Gestion des utilisateurs
 php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_Composante.php
 php bin/console doctrine:fixtures:load --fixtures=src/UserBundle/DataFixtures/ORM/Fake_Utilisateur.php --append
+
+#Etat de l'année
+php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_AnneeEtat.php --append
+
+#Liste des enseignements
+php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_Ue.php --append
+php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_Etape.php --append
 php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_AnneeEtat.php --append
