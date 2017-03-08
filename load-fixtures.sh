2cb15c6310a23ba19c38145c75ed218a93b5a43d
Antoine Mullier <antoinemullier@gmail.com>
2017-03-08 14:58:13 +0100

    Fix sur les scripts de génération de datas

--- load-fixtures.sh
+++ load-fixtures.sh
@@ -10,4 +10,3 @@ php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixture
 #Liste des enseignements
 php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_Ue.php --append
 php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_Etape.php --append
-php bin/console doctrine:fixtures:load --fixtures=src/VisiteurBundle/DataFixtures/ORM/Fake_AnneeEtat.php --append
