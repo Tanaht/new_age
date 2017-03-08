7e5ab1031b0f645f4bc5ad66bc2da5a23de61312
Tanaky <charp.antoine@gmail.com>
2017-03-08 17:35:48 +0100

    Start to work on tokenStorage updates

--- src/UserBundle/Controller/RoleController.php
+++ src/UserBundle/Controller/RoleController.php
@@ -5,7 +5,10 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use UserBundle\Entity\Role;
+use UserBundle\Entity\Utilisateur;
 
 class RoleController extends Controller
 {
@@ -13,5 +16,25 @@ class RoleController extends Controller
     {
         $roles = $this->getDoctrine()->getRepository(Role::class)->findAll();
         return $this->render('@User/Role/dropdown.html.twig', ['roles' => $roles]);
+    }
+
+    public function updateRole(Request $request, Role $role, $routename) {
+        /** @var Utilisateur $user */
+        $user = $this->getUser();
+        $om = $this->getDoctrine()->getManager();
+
+        if(!$user->getRolePosseder()->contains($role)) {
+            /** @var FlashBagInterface $flashBag */
+            $flashBag = $request->getSession()->getFlashBag();
+
+            $flashBag->add('danger', 'Vous ne pouvez pas utiliser ce rôle.');
+        }
+        else {
+            $user->setRoleActuel($role);
+            $om->persist($user);
+            $om->flush();
+            $token = new UsernamePasswordToken($user, null, "db_provider", $user->getRoles());
+            $this->get('security.token_storage')->setToken($token);
+        }
     }
 }
