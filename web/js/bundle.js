d7e1ad4b04feb3534e4e2a4d5c2b4e33999ccbb0
Tanaky <charp.antoine@gmail.com>
2017-03-22 20:08:19 +0100

    Add some client errors constraints

--- web/js/bundle.js
+++ web/js/bundle.js
@@ -77,11 +77,29 @@ module.exports = function($scope, $log, router, config) {
     $scope.annee = "";
     $scope.mois = "";
 
-    $scope.router = router;
-
     $scope.initialize = function(mois, annee) {
         $scope.mois = mois;
         $scope.annee = annee;
+    };
+
+    $scope.generateUrl = function() {
+        return router.generate('visiteur_notifications', { mois: $scope.mois, annee: $scope.annee });
+    };
+
+    $scope.generatedFormClasses = function(form, isButton = false) {
+        if(!isButton) {
+            if (form.$valid)
+                return 'has-success';
+            else
+                return 'has-error';
+
+        }
+        else {
+            if(!form.$valid) {
+                return 'btn-danger disabled';
+            }
+        }
+
     }
 };
 },{}],6:[function(require,module,exports){
