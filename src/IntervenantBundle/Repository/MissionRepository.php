3bbb986a4be89533847075d3d58066a42e8a39cc
Tanaky <charp.antoine@gmail.com>
2017-04-05 17:27:48 +0200

    Fix Query

--- src/IntervenantBundle/Repository/MissionRepository.php
+++ src/IntervenantBundle/Repository/MissionRepository.php
@@ -67,13 +67,13 @@ class MissionRepository extends EntityRepository
             case 'all':
                 break;
             case 'disponible':
-                $qb->andWhere($qb->expr()->eq('missions.statut', Mission::STATUT_LIBRE));
+                $qb->andWhere($qb->expr()->eq('missions.statut', ':statut'))->setParameter(':status', Mission::STATUT_LIBRE);
                 break;
             case 'non-disponible':
-                $qb->andWhere($qb->expr()->eq('missions.statut', Mission::STATUT_FERMEE));
+                $qb->andWhere($qb->expr()->eq('missions.statut', ':statut'))->setParameter(':status', Mission::STATUT_LIBRE);
                 break;
             case 'archive':
-                $qb->andWhere($qb->expr()->eq('missions.statut', Mission::STATUT_ARCHIVEE));
+                $qb->andWhere($qb->expr()->eq('missions.statut', ':statut'))->setParameter(':status', Mission::STATUT_LIBRE);
                 break;
         }
 
